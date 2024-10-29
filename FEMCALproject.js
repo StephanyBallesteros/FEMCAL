125c94269e8b2c0bf7d289cd13d3ca5d84a7e95c
Maria Stephany Ballesteros Camargo <msballesterosc@gmail.com>
2024-10-28 19:19:05 -0500

    ajustes

--- FEMCALproject.js
+++ FEMCALproject.js
@@ -1,20 +1,31 @@
-function validateForm(event){
-    return false;
+function validateForm(email) {
+    const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return re.test(email);
 }
 
-function validateUser(username, password){
-    const message = document.getElementById('message');
+function validateForm(username, password) {
+    const email = document.getElementById('correo electrónico').value;
+    const password = document.getElementById('contraseña').value;
+    let isValid = true;
 
-    const validUsername = 'correo electrónico';
-    const validPassword = '123';
+    if (!validateEmail(email)) {
+        document.getElementById('emailError').innerText = "Correo inválido.";
+        document.getElementById('emailError').style.display = "block";
+        isValid = false;
+    } else {
+        document.getElementById('emailError').style.display = "none";
+    }
 
-    if(validUsername === username && validPassword === password) {
-        message.textContent = 'Inicio de sesión exitoso';
-        message.style.color = 'green';
-        window.location.href = 'paginicio.html'; 
+    if (password.length < 3) {
+        document.getElementById('passwordError').innerText = "La contraseña debe tener al menos 3 caracteres.";
+        document.getElementById('passwordError').style.display = "block";
+        isValid = false;
+    } else {
+        document.getElementById('passwordError').style.display = "none";
+    }
 
-        } else { 
-            message.textContent = 'Usuario o contraseña incorrectos';
-            message.style.color = 'red';
-        }
-}
+    const loginBtn = document.getElementById('iniciar');
+    loginBtn.addEventListener('click', function() {
+         window.location.href = 'http://127.0.0.1:3000/paginicio.html';
+    })
+    return isValid; }
